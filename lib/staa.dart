18bd4594174450067ebc231ff5f0055cd47515a1
nachiketa <nachiketa.p@bizzsetu.com>
2024-03-10 22:47:32 +0530

    new

---
+++ lib/staa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class asas extends StatefulWidget {
+  const asas({super.key});
+
+  @override
+  State<asas> createState() => _asasState();
+}
+
+class _asasState extends State<asas> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
